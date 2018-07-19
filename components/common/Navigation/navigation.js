8ff9ef580e81ad6decce8ac745e993c90a7827a2
miyaBhai <miyaBhaiX1@bitbucket.org>
2018-07-19 23:45:15 +0530

    loader Image and load time optimized

--- components/common/Navigation/navigation.js
+++ components/common/Navigation/navigation.js
@@ -21,8 +21,22 @@ class Nav extends Component{
     }
   }
 
+  registerSW(){
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then(function(registration) {
+          console.log("Service Worker Registered");
+        })
+        .catch(function(err) {
+          console.log("Service Worker Failed to Register", err);
+        })
+    }
+  }
+
   componentDidMount(){
     this.locateme();
+    this.registerSW();
   }
   render(){
     return (
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-// const swPath = path.join(__dirname, '..', 'next', 'service-worker.js');
+const swPath = path.join(__dirname,'sw.js');
 const manifestPath = path.join(__dirname, 'manifest.json');
 
 console.log("Dor ",path.join(__dirname, '/static'));
@@ -19,6 +19,11 @@ app.prepare()
     server.get('/manifest.json', (req, res) => {
       res.setHeader('Cache-Control', 'no-cache');
       res.sendFile(manifestPath);
+    });
+
+    server.get('/service-worker.js', (req, res) => {
+      res.setHeader('Cache-Control', 'no-cache');
+      res.sendFile(swPath);
     });
 
     server.use("/static", express.static(__dirname + "/static", {
@@ -47,12 +52,6 @@ app.prepare()
       const queryParams = { cat_name: req.params.cat_name, pr_name: req.params.pr_name }
       app.render(req, res, actualPage, queryParams)
     })
-
-
-    // server.get('/service-worker.js', (req, res) => {
-    //   res.setHeader('Cache-Control', 'no-cache');
-    //   res.sendFile(swPath);
-    // });
 
     server.get('*', (req, res) => {
       return handle(req, res)
